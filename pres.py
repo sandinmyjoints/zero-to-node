c12e7776aa8327da71e1d3cc23538113ba51fcad
William Bert <william.bert@gmail.com>
2012-10-28 16:30:53 -0400

    Slight code cleanup.

--- pres.py
+++ pres.py
@@ -21,8 +21,6 @@ SECTION = """</section>
 
 notes_re = r"<p>Notes:</p>(.*?)<hr />"
 hr_re    = r"<hr />"
-body_open_re = r'(<body>)'
-body_close_re  = r'(</body>)'
 link_re  = r'http://(.*?)( |</li>)'
 
 md = open("presentation.md").read()
@@ -36,11 +34,8 @@ slides = '<section class="slide">' + pass3 + '</section>'
 
 existing_pres = open("./deck/presentation.html").read()
 
-# insert into boilerplate
-
 final = re.sub(r'(<body.*?>).*(<!-- Begin extension)', '\\1\n' + slides + '\n\\2', existing_pres, flags=re.DOTALL)
-#print "\n\nwriting", final[-2500:-1000]
+#print "\n\nwriting", final
 
-new_pres = open("./deck/presentation.html", "w")
-new_pres.write(final)
-new_pres.close()
+with open("./deck/presentation.html", "w") as new_pres:
+    new_pres.write(final)
